26c4a3f729f187038626ed143cd88e5a413541ed
valantinejaz <boodysaad610@gmail.com>
2025-02-12 21:59:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://collection.cloudinary.com/dfqxnk3nx/60d2bdc4aa7ded759e6851583541faa3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/mitski-my-love-mine-all-mine-official-lyric-video-128-ytshorts.savetube.me_kxsgn8/video/upload/v1234567890/Ahmed Saad 08.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
